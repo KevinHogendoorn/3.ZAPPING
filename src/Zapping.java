1f5bb1ce9e6127bebba2637eb9d7ce494ba8d427
INFORMATICA\dam_hsk <k4631415@gmail.com>
2024-04-30 12:55:51 +0200

    Tercer commit

--- src/Zapping.java
+++ src/Zapping.java
@@ -1,5 +1,35 @@
 public class Zapping {
+    private static final int maxCanal = 99;
+    private static final int minCanal = 0;
+
     public static int cambiarCanal(int origen, int destino){
+        return vueltaCanales(origen, destino);
+    }
+
+    public static int vueltaCanales(int origen, int destino){
+        int clics = calcularClics(origen, destino);
+        int mediaCanal = 49;
+        int restaDestino;
+        int restaOrigen;
+        int resultado;
+
+        if (origen < destino){
+            if (clics > mediaCanal){
+                restaDestino = maxCanal - destino;
+                restaOrigen = Math.abs(minCanal - origen);
+                resultado = restaDestino + restaOrigen;
+            } else {
+                resultado = clics;
+            }
+        } else {
+            restaDestino = maxCanal - origen;
+            restaOrigen = Math.abs(minCanal - destino);
+            resultado = restaDestino + restaOrigen;
+        }
+        return resultado;
+    }
+
+    public static int calcularClics(int origen, int destino){
         int clics;
         if (origen > destino) clics = origen - destino;
         else clics = destino - origen;
--- src/ZappingTest.java
+++ src/ZappingTest.java
@@ -12,4 +12,14 @@ public class ZappingTest {
     public void testSalida1(){
         assertEquals(1, Zapping.cambiarCanal(1, 2));
     }
+
+    @Test
+    public void testSalida20(){
+        assertEquals(20, Zapping.cambiarCanal(10, 30));
+    }
+
+    @Test
+    public void testSalida2(){
+        assertEquals(2, Zapping.cambiarCanal(2, 99));
+    }
 }
